7479fbf009a05d1b4042663576adc693658e5cc3
DanishAbbas <danish.abbas@arbisoft.com>
2023-08-08 17:23:50 +0500

    Trying different things

--- maze.py
+++ maze.py
@@ -3,11 +3,12 @@ from collections import deque
 visited = set()
 que = deque()
 result = []
+end = None
 
 
 def maze_solver(maze):
-    global que
-    global result
+    global que, result, end
+
     # Calculating the starting position
     for i in range(len(maze)):
         for j in range(len(maze[0])):
@@ -15,16 +16,23 @@ def maze_solver(maze):
                 start = (i, j)
                 que = deque([(start, '')])  # Giving the que empty set a start value
                 break
+    # Calculating the psotion of X
+    for p in range(len(maze)):
+        for q in range(len(maze[0])):
+            if maze[p][q] == 'X':
+                end = (p, q)
+                break
 
     def move(x, y, direction):  # This function moves the ball in the available direction
-        if direction == 'N':
-            return x - 1, y
-        if direction == 'W':
-            return x, y - 1
-        if direction == 'S':
-            return x + 1, y
-        if direction == 'E':
-            return x, y + 1
+        if (x, y) != end:
+            if direction == 'N':
+                return x - 1, y
+            if direction == 'W':
+                return x, y - 1
+            if direction == 'S':
+                return x + 1, y
+            if direction == 'E':
+                return x, y + 1
 
     while que:
         (x, y), path = que.popleft()
@@ -35,26 +43,27 @@ def maze_solver(maze):
             if (
                     0 <= nx < len(maze) and
                     0 <= ny < len(maze[0]) and
-                    isinstance(maze[nx][ny], int) and
-                    (nx, ny) not in visited
+                    (nx, ny) not in visited and
+                    (nx, ny) != end
             ):
+                print(nx, ny)
+                print(maze[nx][ny])
                 if (maze[nx][ny] & wall_bit) == 0:
                     new_path = path + direction
+                    print(new_path)
                     que.append(((nx, ny), new_path))
                     visited.add((nx, ny))
-                    result = new_path
+                    result.append(new_path)
 
-    if result:
-        return list(result)
-    else:
-        return None  # No possible solution
+                else:
+                    return list(result)
 
 
 example = (
-    ('B', 2, 5, 4),
-    (4, 15, 11, 5),
-    (10, 9, 6, 8),
-    (12, 20, 7, 'X')
+    (4, 2, 5, 4),
+    (4, 15, 11, 1),
+    ('B', 9, 6, 8),
+    (12, 7, 7, 'X')
 )
 
 print(maze_solver(example))
