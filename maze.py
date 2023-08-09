d71c6b122669da0252bf0c65c7a9401cd35f68f7
DanishAbbas <danish.abbas@arbisoft.com>
2023-08-09 12:33:29 +0500

    The best Version so far

--- maze.py
+++ maze.py
@@ -1,69 +1,75 @@
 from collections import deque
 
-visited = set()
-que = deque()
-result = []
-end = None
-
 
 def maze_solver(maze):
-    global que, result, end
+    # Initialising variables
+    visited = set()
+    queue = deque()
+    result = None
+    end = None
+    reached_goal = False
 
     # Calculating the starting position
     for i in range(len(maze)):
         for j in range(len(maze[0])):
             if maze[i][j] == 'B':
                 start = (i, j)
-                que = deque([(start, '')])  # Giving the que empty set a start value
+                queue = deque([(start, '')])  # Giving the que empty set a start value
                 break
-    # Calculating the psotion of X
+    # Calculating the position of X
     for p in range(len(maze)):
         for q in range(len(maze[0])):
             if maze[p][q] == 'X':
                 end = (p, q)
                 break
+    # If the user gives no starting position the program ends here
+    if end is None:
+        return None
 
     def move(x, y, direction):  # This function moves the ball in the available direction
-        if (x, y) != end:
-            if direction == 'N':
-                return x - 1, y
-            if direction == 'W':
-                return x, y - 1
-            if direction == 'S':
-                return x + 1, y
-            if direction == 'E':
-                return x, y + 1
 
-    while que:
-        (x, y), path = que.popleft()
+        if direction == 'N':
+            return x - 1, y
+        if direction == 'W':
+            return x, y - 1
+        if direction == 'S':
+            return x + 1, y
+        if direction == 'E':
+            return x, y + 1
+
+    while queue:
+        (x, y), path = queue.popleft()  # Getting the co-ordinates of the current position and path travelled
         visited.add((x, y))
 
         for direction, wall_bit in zip(['N', 'W', 'S', 'E'], [8, 4, 2, 1]):
             nx, ny = move(x, y, direction)
-            if (
+            if (nx, ny) == end:  # Terminating the function once 'X' is reached.
+                reached_goal = True
+                break
+            elif (  # Calculating available directions
                     0 <= nx < len(maze) and
                     0 <= ny < len(maze[0]) and
-                    (nx, ny) not in visited and
-                    (nx, ny) != end
-            ):
-                print(nx, ny)
-                print(maze[nx][ny])
+                    (nx, ny) not in visited
+
+            ):  # Making a check for walls using Binary data
                 if (maze[nx][ny] & wall_bit) == 0:
                     new_path = path + direction
-                    print(new_path)
-                    que.append(((nx, ny), new_path))
+                    queue.append(((nx, ny), new_path))
                     visited.add((nx, ny))
-                    result.append(new_path)
+                    result = new_path  # Getting the shortest available path
 
-                else:
-                    return list(result)
+    if reached_goal:
+        return list(result)
+    else:
+        return None  # No possible solution
 
 
+# An example set
 example = (
-    (4, 2, 5, 4),
-    (4, 15, 11, 1),
-    ('B', 9, 6, 8),
-    (12, 7, 7, 'X')
+    ('B', 2, 3, 4),
+    (5, 6, 7, 8),
+    (9, 10, 11, 12),
+    (13, 14, 14, 'X')
 )
 
 print(maze_solver(example))
