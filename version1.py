90c7cfc121c115fe84e16f0e2f6e08fbafdffb23
DanishAbbas <danish.abbas@arbisoft.com>
2023-08-13 17:11:38 +0500

    MY best version

--- version1.py
+++ version1.py
@@ -17,12 +17,16 @@ def maze_solver(maze):
         for j in range(len(maze[0])):
             if maze[i][j] == 'B':
                 start = (i, j)
+                maze = variable_positions(maze, i, j)
+                print(maze)
                 queue = deque([(start, '')])  # Giving the que empty set a start value
                 break
     # Calculating the position of X
     for p in range(len(maze)):
         for q in range(len(maze[0])):
             if maze[p][q] == 'X':
+                maze = variable_positions(maze, p, q)
+                print(maze)
                 end = (p, q)
                 break
     # If the user gives no starting position the program ends here
@@ -33,20 +37,23 @@ def maze_solver(maze):
         while queue:
             (x, y), path = queue.popleft()  # Getting the co-ordinates of the current position and path travelled
             visited.add((x, y))
+            if (x, y) == end:
+                break
             for direction, wall_bit in zip(directions, wall_bits):
                 nx, ny = move(x, y, direction)
-                if (nx, ny) == end:  # Terminating the function once 'X' is reached.
-                    reached_goal = True
-                    result = path + direction  # Getting the shortest available path
-                    break
-                elif (  # Calculating available directions
-                        (nx, ny) not in visited and
-                        0 <= nx < len(maze) and
-                        0 <= ny < len(maze[0])
+                if (maze[x][y] & wall_bit) == 0:  # Checking if the current cell has walls in the direction
+                    if (nx, ny) == end:  # Terminating the function once 'X' is reached.
+                        reached_goal = True
+                        result = path + direction  # Getting the shortest available path
+                        break
+                    elif (  # Calculating available directions
+                            (nx, ny) not in visited and
+                            0 <= nx < len(maze) and
+                            0 <= ny < len(maze[0]) and
+                            (maze[nx][ny] & wall_bit) == 0
+                    ):
 
-                ):  # Making a check for walls using Binary data
-
-                    if (maze[nx][ny] & wall_bit) == 0:
+                        print(maze[nx][ny])
                         new_path = path + direction
                         queue.append(((nx, ny), new_path))
                         visited.add((nx, ny))
@@ -56,9 +63,12 @@ def maze_solver(maze):
             return list(result)
         else:
             rotated_cell = rotate_cell(maze[nx][ny])
+            print(rotated_cell)
             maze = update_cell(maze, rotated_cell, nx, ny)
+            print(maze)
             iterations += 1
             queue.append(((nx, ny), new_path))
+            print(queue)
 
     return None
     # No possible solution
@@ -88,6 +98,15 @@ def update_cell(maze, rotated_cell, nx, ny):
     updated_maze[nx] = list(updated_maze[nx])
     updated_maze[nx][ny] = rotated_cell
     updated_maze[nx] = tuple(updated_maze[nx])
+    updated_maze = tuple(updated_maze)
+    return updated_maze
+
+
+def variable_positions(maze, a, b):
+    updated_maze = list(maze)
+    updated_maze[a] = list(updated_maze[a])
+    updated_maze[a][b] = 0
+    updated_maze[a] = tuple(updated_maze[a])
     updated_maze = tuple(updated_maze)
     return updated_maze
 
